143c1bfbbbf7af544878543f7fbfc61cf28fb931
Antonia Eckert <eckert.antonia@web.de>
2025-01-07 11:18:06 +0100

    ADD: InvitationChoreo.py, led-control.py

---
+++ server/led-control.py
@@ -0,0 +1,11 @@
+
+class LEDControl:
+
+    def show_string(self, robot, string, color):
+        pass
+
+    def show_character(self, robot, character, color):
+        pass
+
+    def show_grouping(self, robot, color):
+        pass
