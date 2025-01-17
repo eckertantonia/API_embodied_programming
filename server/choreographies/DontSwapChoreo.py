1a8e22c1c62103d4325b76480e2f12a8335f2bce
Antonia Eckert <antonia.eckert@student.hs-rm.de>
2025-01-17 11:39:43 +0100

    ADD:logic in SwapChoreo.py and DontSwapChoreo.py

--- server/choreographies/DontSwapChoreo.py
+++ server/choreographies/DontSwapChoreo.py
@@ -1,9 +1,30 @@
 from server.choreographies.ChoreographyInterface import ChoreographyInterface
+from server.choreographies.SwapChoreo import TooManyRobotsForChoreoException
+from server.ledcontrol import LEDControl
+from server.movement.movement_strategies.CompareNoChangeStrategy import CompareNoChangeStrategy
+from server.movement.movement_strategies.MoveForwardStrategy import MoveForwardStrategy
 
 
 class DontSwapChoreo(ChoreographyInterface):
     def __init__(self):
-        pass
+        self.ledcontrol = LEDControl()
 
     def start_choreo(self, robot_group, values):
-        pass
+
+        if len(robot_group) != 2:
+            raise TooManyRobotsForChoreoException(robot_group)
+
+        end_points = [robot_group[1].position, robot_group[0].position]
+
+        # compare no change
+        compare_no_change = CompareNoChangeStrategy()
+        compare_no_change.drive(robot_group, [])
+        for bolt in robot_group:
+            self.ledcontrol.green_character(bolt, bolt.value)
+
+        # zurück fahren
+        move_back = MoveForwardStrategy()
+        move_back.drive(robot_group, [end_points[-1], end_points[0]])
+
+        for bolt in robot_group:
+            self.ledcontrol.show_character(bolt, bolt.value)
--- server/choreographies/SwapChoreo.py
+++ server/choreographies/SwapChoreo.py
@@ -1,9 +1,39 @@
 from server.choreographies.ChoreographyInterface import ChoreographyInterface
+from server.ledcontrol import LEDControl
+from server.movement.movement_strategies.CompareWithChangeStrategy import CompareWithChangeStrategy
+from server.movement.movement_strategies.MoveForwardStrategy import MoveForwardStrategy
 
 
 class SwapChoreo(ChoreographyInterface):
     def __init__(self):
-        pass
+        self.ledcontrol = LEDControl()
 
     def start_choreo(self, robot_group, values):
+
+        if len(robot_group) != 2:
+            raise TooManyRobotsForChoreoException(robot_group)
+
+        end_points = [robot_group[1].position, robot_group[0].position]
+
+        # compare with change
+        compare_with_change = CompareWithChangeStrategy()
+        compare_with_change.drive(robot_group, [])
+
+        for bolt in robot_group:
+            self.ledcontrol.show_character(bolt, bolt.value)
+
+        # zurück fahren
+        move_back = MoveForwardStrategy()
+        move_back.drive(robot_group, end_points)
+
+        for bolt in robot_group:
+            self.ledcontrol.show_character(bolt, bolt.value)
+
         pass
+
+class TooManyRobotsForChoreoException(Exception):
+    def __init__(self, robots):
+        self.len_robots = len(robots)
+        self.message = (f"TooManyRobotsForChoreoException: "
+                        f"There are too many robots for this Choreography.")
+        super().__init__(self.message)
