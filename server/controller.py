023af2661db1ef95706426210ea84623e7f95050
Antonia Eckert <antonia.eckert@student.hs-rm.de>
2025-01-18 10:20:49 +0100

    REFACTOR: controller.py

--- server/controller.py
+++ server/controller.py
@@ -4,7 +4,7 @@ from manager import Manager
 class Controller:
     def __init__(self):
         self.manager = Manager()
-        self.initial_connect = False
+        self._initial_connect = False
 
     def process_command(self, payload):
         choreography = payload.get("choreography")
@@ -12,36 +12,36 @@ class Controller:
         message = payload.get("message", "")
         response = ""
 
-        if not self.initial_connect:
-            self.initial_connect = True
-            response = self.control_initial_connect(values, choreography)
+        if not self._initial_connect:
+            self._initial_connect = True
+            response = self._control_initial_connect(values, choreography)
 
         elif message == "start":
-            response = self.control_start()
+            response = self._control_start()
 
         elif message == "stopp":
-            return self.control_disconnect()
+            return self._control_disconnect()
 
         elif message == "custom":
-            return self.control_custom_start(values, choreography)
+            return self._control_custom_start(values, choreography)
         else:
             return "Unknown command"
 
         return response
 
-    def control_initial_connect(self, values, choreography):
+    def _control_initial_connect(self, values, choreography):
         self.manager.values = values
         self.manager.choreo = choreography
         position_string = self.manager.connect_bolts()
 
         return f"Ordne die Roboter in folgender Reihenfolge an: \n" + position_string
 
-    def control_disconnect(self):
+    def _control_disconnect(self):
         self.manager.close_api()
         return "Apis closed."
 
-    def control_start(self):
+    def _control_start(self):
         return self.manager.start()
 
-    def control_custom_start(self, values, choreography):
+    def _control_custom_start(self, values, choreography):
         return self.manager.start(values, choreography)
